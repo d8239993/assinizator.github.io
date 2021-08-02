250f79604b6e723de976dfc639cc623c4197fd76
Даниил Орлов <54530217+d8239993@users.noreply.github.com>
2021-08-02 20:34:13 +0300

    Update form.php

--- form.php
+++ form.php
@@ -3,11 +3,11 @@
 header('Access-Control-Allow-Origin: *');
 
 /* замените электронную почту на действующую почту,  с которой вам будет приходить письмо  */
-$headers = "From: from@webhookservesite.ru";​ 
+$headers = "From: 8239993@mail.ru";​ 
 ​
 $message = print_r($_POST,true);
 /* замените электронную почту на вашу действующую почту,  на которую вам будет приходить письмо  */
-@mail('to@mail.ru', 'Tilda TEST', $message, $headers);
+@mail('8239993@mail.ru', 'Tilda TEST', $message, $headers);
 
 echo "ok";
 
